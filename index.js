aa4f0f2a7c4f84885df18175331662288bdb6b44
Jimmy <dm.a.uvarov@gmail.com>
2022-01-23 18:40:37 -0500

    commit tree 8c59ad74f842baac59343bed986900176c546bd5

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
+
 function isPalindrome(word) {
   // Write your algorithm here
+
+  // "drow" is reverse "word"
+  const drow = word.split("").reverse().join("");
+
+  // comparing reversed and original strings
+  return (word.toUpperCase() === drow.toUpperCase())
 }
 
 /* 
   Add your pseudocode here
+ 
+ * Assign to a constant "drow" (inverted incomed string), incomed parameter "word" type of string that will be
+ 1) splitted to a single characters into array that will be:
+ 
+ 2) reversed (1srt element of array will be the last and second will be laat -1 and
+   so on)  and will be 
+
+
+ 3) constructed to a string from charaters of that array.
+
+* return Boolean result of comparing inverted and original string (both uppercased)
+  
 */
 
 /*
   Add written explanation of your solution here
+
+  we take original string, reverse it and comparing with original string.
+  if strings are the same, then original string is palindrome,
+
+  to uppercased() was added for case insensitivity.
+
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -20,6 +46,27 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  // my custom tests
+  console.log(" \n\n");
+  console.log("1:Expecting: false");
+  console.log("=>", isPalindrome("param"));
+
+  console.log("2:Expecting: true");
+  console.log("=>", isPalindrome("teneT"));
+
+  console.log("3:Expecting: true");
+  console.log("=>", isPalindrome("woow"));
+
+  console.log("4:Expecting: true");
+  console.log("=>", isPalindrome("wow wow"));
+
+
+
+
+
+
+
 }
 
 module.exports = isPalindrome;
